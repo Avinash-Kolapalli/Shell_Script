2a8250f27e039b8695223522a12d27b722ba6d42
Avinash-Kolapalli <kolapalliavinash@gmail.com>
2026-06-20 05:56:22 -0600

    Add Practice-01.sh with a sample conversation

--- repos/Practice-01.sh
+++ repos/Practice-01.sh
@@ -2,5 +2,15 @@
 
 #This script is used only for testing purposes, it will be removed in the future.
 
-echo "Hello, Welcome!"
-echo "Have a nice day!"
+person1="John"
+person2="Jane" 
+
+echo "This is conversation between $person1 and $person2."
+echo "$person1: Hi $person2, how are you?"
+echo "$person2: Hi $person1, I'm doing well, thanks! How about you?"
+echo "$person1: I'm good too, thanks for asking!"
+echo "End of conversation."
+
+git add Practice-01.sh
+git commit -m "Add Practice-01.sh with a sample conversation"
+git push origin main
